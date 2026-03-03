b4a31803bdea7af523ee61a4b92ccdd78114a6db
Claus Eggers Sørensen <es@forthehearts.net>
2026-03-03 09:53:03 +0100

    JS files in gitignore

--- http_root/shared/AppShell.d.ts
+++
@@ -1,9 +0,0 @@
-export interface AppInstance {
-    fontLoader?: {
-        restoreFromSession?: () => Promise<void>;
-    };
-    destroy?: () => void;
-}
-export type AppConstructor = new () => AppInstance;
-export declare function switchApp(key: string): Promise<void>;
-//# sourceMappingURL=AppShell.d.ts.map
--- http_root/wordmaster/TextFitter.d.ts
+++
@@ -1,9 +0,0 @@
-export interface TextFitterOptions {
-    paddingPercentage?: number;
-}
-export declare class TextFitter {
-    paddingPercentage: number;
-    constructor({ paddingPercentage }?: TextFitterOptions);
-    fitText(element: HTMLElement, container: HTMLElement): void;
-}
-//# sourceMappingURL=TextFitter.d.ts.map
